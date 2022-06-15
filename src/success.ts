29571d971852a5d78ca2527b872e279f0fc15c25
guiadco <guiadco@geekhomeinside.com>
2022-06-15 17:09:00 +0200

    chore(js): clean code

--- src/success.ts
+++ src/success.ts
@@ -1,14 +1,7 @@
 import SemanticReleaseError from "@semantic-release/error";
 import { getDiscordVars } from "./lib/getDiscordVars";
-// import { postMessage } from "./lib/postMessage";
-import { getConfigToUse } from "./lib/getConfigToUse";
-import { repositoryUrl } from "./lib/getRepoInfo";
 
 export async function success(context) {
   const { logger } = context;
-
   const { discordWebhook, discordWebhookEnVar, discordUsername, discordUsernameEnVar } = getDiscordVars();
-  const foo = getConfigToUse;
-  const bar = repositoryUrl;
-
 }
